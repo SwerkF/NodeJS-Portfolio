4a9d10ca01e929702a249bc7eca8ff7f51d7d753
Oliwer Skweres <swerk.pro@gmail.com>
2022-11-21 20:37:16 +0100

    <Initial Commit>

---
+++ src/js/module.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/04BkUuAB3W-akDj1/scene.splinecode');
